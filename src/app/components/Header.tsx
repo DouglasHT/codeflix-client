9ba196322d9c1d450ea7cc5c99a54a253dd5a6f9
Douglas H. Tripoloni <dhtripoloni@hotmail.com>
2023-08-03 17:30:36 -0300

    header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -13,12 +13,19 @@ export default function Header(){
         w-full
         items-center
         justify-between
+        bg-black
         px-4
         py-4
         
         lg:px-10
         lg:py-6'>
-            
+
+            <div className='
+            flex
+            items-center
+            space-x-2
+            md:space-x-4'>
+
 
         <img src = "https://rb.gy/ulxxee"
             alt = "netflix"
@@ -33,6 +40,7 @@ export default function Header(){
                 <li>My List</li>
             </ul>
 
+            </div>
             </header>
     )
 }
