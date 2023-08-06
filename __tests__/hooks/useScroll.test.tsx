0ab1afb6c9ca4c10e76bb7824997a9eb0f3b3fa8
Douglas H. Tripoloni <dhtripoloni@hotmail.com>
2023-08-06 00:09:18 -0300

    test

---
+++ __tests__/hooks/useScroll.test.tsx
@@ -0,0 +1,28 @@
+import { useScroll } from "app/hooks/useScroll";
+import { renderHook, act } from "@testing-library/react";
+
+describe("useScroll", () => {
+    it("should be", () => {
+        expect(true).toBeTruthy();
+    });
+})
+
+// describe("useScroll", () => {
+// 	it("should respond to scroll events", () => {
+// 		const { result } = renderHook(() => useScroll());
+
+// 		act(() => {
+// 			global.window.scrollY = 100;
+// 			global.window.dispatchEvent(new Event("scroll"));
+// 		});
+
+// 		expect(result.current).toBe(true);
+
+//         act(() => {
+// 			global.window.scrollY = 100;
+// 			global.window.dispatchEvent(new Event("scroll"));
+// 		});
+
+// 		expect(result.current).toBe(false);
+// 	});
+// });
