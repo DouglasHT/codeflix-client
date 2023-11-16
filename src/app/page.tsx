11b426672895f603db0fdcc503afcce13a20f49f
Douglas H. Tripoloni <dhtripoloni@hotmail.com>
2023-11-16 11:48:39 -0300

    feature: img sugestions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,6 +33,23 @@ export default function Home() {
             More Info
           </button>
         </div>
+        <div className='flex-col space-y-4'>
+          <div className='flex'>
+            <h2 className='-ml-2 inline-flex items-center text-2xl font-bold'>Feature</h2>
+          </div>
+          <div className='-ml-8 flex space-x-4 overflow-x-scroll p-6 scrollbar-hide'>
+          {[1,2,3,4,5,5,5,5].map((index)=> (
+            <div key={index} className='group relative h-28 min-w-[200px] cursor-pointer rounded bg-gradient-to-t from-transparent to-black transition-transform duration-200 ease-out hover:opacity-100 md:h-36 md:min-w-[260px] md:hover:scale-110'>
+              <Image              
+              src={`/item_${index}.png`}          
+              fill={true}
+              alt='MAID'
+              className='rounded'
+            />
+          </div>
+          ))}
+          </div>
+        </div>
       </main>
     </div>
   );
