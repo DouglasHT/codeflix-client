7f133a9a4bad623084406f369c027deec2f30fde
Douglas H. Tripoloni <dhtripoloni@hotmail.com>
2023-11-23 14:44:21 -0300

    feature: new field at login form, init new pages forgot and register

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,10 @@
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<div
+			style={{ backgroundImage: "url('/background.jpg')" }}
+			className="bg-cover bg-center bg-no-repeat text-white opacity-80"
+		>
+			<div className="flex min-h-screen flex-col items-center justify-center py-2">{children}</div>
+		</div>
+	);
+}
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,33 +1,45 @@
 export default function LoginPage() {
 	return (
-		<div
-			style={{ backgroundImage: "url('/background.jpg')" }}
-			className="bg-cover bg-center bg-no-repeat text-white opacity-80"
-		>
-			<div className="flex min-h-screen flex-col items-center justify-center py-2">
-				<form className="flex w-full flex-col space-y-4 bg-[#141414] bg-opacity-90 px-4 py-8 shadow-lg">
-					<div className="flex flex-col items-center space-y-4">
-						<h1 className="text-3xl font-bold">Login</h1>
-						<p className="text-sm text-gray-500">
-							Don't have an account?
-							<a className="text-red-500 hover:underline">Sign up</a>
-						</p>
-					</div>
-					<div className="mt-8 flex flex-col space-y-4">
-						<label htmlFor="email" className="block text-sm font-medium">
-							Email
-						</label>
-						<input
-							id="email"
-							name="email"
-							type="email"
-							autoComplete="email"
-							required
-							className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-red-500 focus:ring-red-500"
-						/>
-					</div>
-				</form>
+		<form className="flex w-full max-w-md flex-col rounded space-y-4 bg-[#141414] bg-opacity-90 px-4 py-8 shadow-lg">
+			<div className="flex flex-col items-center space-y-4">
+				<h1 className="text-3xl font-bold">Login</h1>
+				<p className="text-sm text-gray-500">
+					New to the app?
+					<a className="text-red-500 hover:underline"> Sign up</a>
+				</p>
 			</div>
-		</div>
+			<div className="mt-8 flex flex-col space-y-4">
+				<div className="flex flex-col space-y-1">
+					<label htmlFor="email" className="text-sm font-bold text-gray-500">
+						Email
+					</label>
+					<input
+						type="email"
+						id="email"
+						placeholder="Enter your email"
+						className="rounded-lg border-gray-600 bg-gray-700 px-4 py-2 Focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent"
+					/>
+				</div>
+				<div className="flex flex-col space-y-1">
+					<label htmlFor="password" className="text-sm font-bold text-gray-500">
+						Password
+					</label>
+					<input
+						type="password"
+						id="password"
+						placeholder="Enter your password"
+						className="rounded-lg border-gray-600 bg-gray-700 px-4 py-2 Focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent"
+					/>
+				</div>
+				<div className="flex flex-col-reverse space-y-2 pt-2 sm:flex-row sm:space-x-2 sm:space-y-0">
+					<button
+						className="flex w-full items-center justify-center rounded-lg bg-red-500 hover:bg-red-600 px-4 py-2 text-sm semi-bold text-white sm:w-auto sm:px-8"
+						type="submit"
+					>
+						Submit
+					</button>
+				</div>
+			</div>
+		</form>
 	);
 }
