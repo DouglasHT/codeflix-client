e7c23d747644d663034141855c197d50ebdeed23
Douglas H. Tripoloni <dhtripoloni@hotmail.com>
2023-11-28 10:38:05 -0300

    feature: login component for form and input field

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,45 +1,5 @@
-export default function LoginPage() {
-	return (
-		<form className="flex w-full max-w-md flex-col rounded space-y-4 bg-[#141414] bg-opacity-90 px-4 py-8 shadow-lg">
-			<div className="flex flex-col items-center space-y-4">
-				<h1 className="text-3xl font-bold">Login</h1>
-				<p className="text-sm text-gray-500">
-					New to the app?
-					<a className="text-red-500 hover:underline"> Sign up</a>
-				</p>
-			</div>
-			<div className="mt-8 flex flex-col space-y-4">
-				<div className="flex flex-col space-y-1">
-					<label htmlFor="email" className="text-sm font-bold text-gray-500">
-						Email
-					</label>
-					<input
-						type="email"
-						id="email"
-						placeholder="Enter your email"
-						className="rounded-lg border-gray-600 bg-gray-700 px-4 py-2 Focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent"
-					/>
-				</div>
-				<div className="flex flex-col space-y-1">
-					<label htmlFor="password" className="text-sm font-bold text-gray-500">
-						Password
-					</label>
-					<input
-						type="password"
-						id="password"
-						placeholder="Enter your password"
-						className="rounded-lg border-gray-600 bg-gray-700 px-4 py-2 Focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent"
-					/>
-				</div>
-				<div className="flex flex-col-reverse space-y-2 pt-2 sm:flex-row sm:space-x-2 sm:space-y-0">
-					<button
-						className="flex w-full items-center justify-center rounded-lg bg-red-500 hover:bg-red-600 px-4 py-2 text-sm semi-bold text-white sm:w-auto sm:px-8"
-						type="submit"
-					>
-						Submit
-					</button>
-				</div>
-			</div>
-		</form>
-	);
+import LoginForm from "./LoginForm";
+
+export default function LoginPge() {
+	return <LoginForm />;
 }
